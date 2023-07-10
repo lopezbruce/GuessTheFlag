8dcbc21244cb3fed97dbae1e986eac2ff8716a19
Bruce Lopez <lopez.bruce@gmail.com>
2023-07-10 10:45:20 -0400

    Create a custom ViewModifier and accompanying View extension that makes a view have a large bold font and white color

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -18,6 +18,21 @@ struct FlagImage: View {
     }
 }
 
+struct Title: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.largeTitle.weight(.bold))
+            .foregroundColor(.white)
+            .padding()
+    }
+}
+
+extension View {
+    func titleStyle() -> some View {
+        modifier(Title())
+    }
+}
+
 struct ContentView: View {
     @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
     @State private var correctAnswer = Int.random(in: 0...2)
@@ -39,8 +54,7 @@ struct ContentView: View {
                 Spacer()
                 
                 Text("Guess the Flag")
-                    .font(.largeTitle.weight(.bold))
-                    .foregroundColor(.white)
+                    .titleStyle()
                 
                 VStack(spacing: 15){
                     VStack {
